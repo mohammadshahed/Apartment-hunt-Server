2d1d2a924e271b0626e800f041be70b716e27d0c
mohammadshahed <silentshahed@gmail.com>
2020-11-28 23:09:15 +0600

    dot.env updated

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 // MongoDB Connect
-const uri = `mongodb+srv://apartmenthunt:aIwpJXgOtmCEgMgV@cluster0.sgixs.mongodb.net/apartmentHunt?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgixs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
